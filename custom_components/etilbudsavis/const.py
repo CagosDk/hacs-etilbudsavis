b6ec9fdfb698a1e8f8897333dac62e9dfb400eb1
CagosDk <48596772+CagosDk@users.noreply.github.com>
2026-06-06 15:42:01 +0200

    Initial release

---
+++ custom_components/etilbudsavis/const.py
@@ -0,0 +1,10 @@
+"""Constants for eTilbudsavis integration."""
+
+DOMAIN = "etilbudsavis"
+
+CONF_TOKEN = "token"
+CONF_EMAIL = "email"
+CONF_SHOPPING_LIST_ID = "shopping_list_id"
+CONF_OTP_ID = "otp_id"
+
+SCAN_INTERVAL_MINUTES = 5
